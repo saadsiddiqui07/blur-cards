a03c7e17b751a34b9c2f0cf9173de76934bd19cb
Saad Heisentech <saadsiddiqui@Saads-MacBook-Air.local>
2023-12-29 20:31:17 +0530

    code rafactor

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import {
   Platform,
   SafeAreaView,
   StyleSheet,
-  Text,
   View,
 } from "react-native";
 import Animated, {
@@ -15,18 +14,9 @@ import Animated, {
   useAnimatedStyle,
   useSharedValue,
 } from "react-native-reanimated";
+import data from "./data";
 
 const { width } = Dimensions.get("screen");
-
-const data = [
-  "https://cdn.dribbble.com/users/3281732/screenshots/11192830/media/7690704fa8f0566d572a085637dd1eee.jpg?compress=1&resize=1200x1200",
-  "https://cdn.dribbble.com/users/3281732/screenshots/13130602/media/592ccac0a949b39f058a297fd1faa38e.jpg?compress=1&resize=1200x1200",
-  "https://cdn.dribbble.com/users/3281732/screenshots/9165292/media/ccbfbce040e1941972dbc6a378c35e98.jpg?compress=1&resize=1200x1200",
-  "https://cdn.dribbble.com/users/3281732/screenshots/11205211/media/44c854b0a6e381340fbefe276e03e8e4.jpg?compress=1&resize=1200x1200",
-  "https://cdn.dribbble.com/users/3281732/screenshots/7003560/media/48d5ac3503d204751a2890ba82cc42ad.jpg?compress=1&resize=1200x1200",
-  "https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=1200x1200",
-  "https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200",
-];
 
 const imageWidth = width * 0.7;
 const imageH = imageWidth * 1.54;
@@ -72,19 +62,6 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
-      {/* <Text
-        style={{
-          position: "absolute",
-          top: "10%",
-          left: 10,
-          color: "#fff",
-          fontWeight: "800",
-          zIndex: 1,
-          fontSize: 22,
-        }}
-      >
-        SOME TITLE
-      </Text> */}
       <View style={StyleSheet.absoluteFillObject}>
         {data.map((item, idx) => {
           return (
